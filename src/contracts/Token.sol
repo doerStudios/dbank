5cd2dd33de6a73e985d7646cbda36192a814b161
doerstudios <contact.doerstudios@gmail.com>
2021-07-20 12:02:35 +0800

    project setup

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,21 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity >=0.6.0 <0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+  //  add minter variable
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
+  constructor() public payable ERC20("Brick Coin", "BRK") {
+    //  asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
 
   function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
+    //  check if msg.sender have minter role
+    require(msg.sender == minter, 'Error, msg.sender does not have minter role');
 		_mint(account, amount);
 	}
 }
